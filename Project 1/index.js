d54b3d94b7b279137cbcf5c47f14b44d403f7e85
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-07 12:49:58 +0200

    Finished Debugging

--- Project 1/index.js
+++ Project 1/index.js
@@ -3,31 +3,32 @@ const txtTrollMsg = document.getElementById('txtTrollMsg');
 const btnNewTrollMsg = document.getElementById('btnNewTrollMsg');
 
 function updateTrollbox(trollPost) {
-    var trollData = '<p><b>' + trollPost.troll;
-    trollData += '</b> said: ' + trollPost.message;
-    trollData += ' - <small>' + trollPost.date + '</small><p>';
+  var trollData = '<p><b>' + trollPost.troll;
+  trollData += '</b> said: ' + trollPost.message;
+  trollData += ' - <small>' + trollPost.date + '</small><p>';
 
-    // inject into HTML list element
-    var currentTrollbox = document.getElementById('trollbox').innerHTML;
-    document.getElementById('trollbox').innerHTML = currentTrollbox + trollData;
+  // inject into HTML list element
+  var currentTrollbox = document.getElementById('trollbox').innerHTML;
+  document.getElementById('trollbox').innerHTML = currentTrollbox + trollData;
+  //   console.info('info log');
 }
 
 // new trollbox entry
-btnNewTrollMsg.addEventListener('click', e => {
-    const troll = txtTrollName.value;
-    const msg = txtTrollMsg.value;
-    const now = new Date();
-    const trollPost = {
-        troll: troll,
-        message: msg,
-        date: now.toUTCString()
-    };
+btnNewTrollMsg.addEventListener('click', (e) => {
+  const troll = txtTrollName.value;
+  const msg = txtTrollMsg.value;
+  const now = new Date();
+  const trollPost = {
+    troll: troll,
+    message: msg,
+    date: now.toUTCString(),
+  };
 
-    // update posts
-    updateTrollbox(trollPost);
+  // update posts
+  updateTrollbox(trollPost);
 
-    // clear textbox
-    document.getElementById('txtTrollName').value = '';
-    document.getElementById('txtTrollMsg').value = '';
-    console.clear();
+  // clear textbox
+  document.getElementById('txtTrollName').value = '';
+  document.getElementById('txtTrollMsg').value = '';
+  console.clear();
 });
